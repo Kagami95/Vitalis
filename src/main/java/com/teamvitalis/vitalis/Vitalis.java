7735add3513f55fc5961512886142bc31f6793ac
jedk1 <jedkristian@live.com>
2016-04-28 16:46:31 +0100

    Initial Commit

---
+++ src/main/java/com/teamvitalis/vitalis/Vitalis.java
@@ -0,0 +1,17 @@
+package com.teamvitalis.vitalis;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Vitalis extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		//TODO
+	}
+	
+	@Override
+	public void onDisable() {
+		//TODO
+	}
+
+}
